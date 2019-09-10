aec82299dbe94fef2802e6b73689d77818ccdbb2
Carl Hester <crucialcarl@gmail.com>
2019-09-10 21:57:48 +0000

    hash and output filename

--- watchpagego.go
+++ watchpagego.go
@@ -4,14 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	//"io"
+	"crypto/md5"
 	"crypto/tls"
+	"encoding/hex"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-func LinesInFile(fileName string) []string {
+func linesInFile(fileName string) []string {
 	// Reads and returns lines from fileName
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -29,25 +32,40 @@ func LinesInFile(fileName string) []string {
 	return result
 }
 
-func main() {
+func getRespCodeAndSiteData(site string) (int, string) {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	// Fetches a site and returns response code and data
+	response, err := http.Get(site)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
 
+	httpResponseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	respCode := int(response.StatusCode)
+	respData := string(httpResponseData)
+	return respCode, respData
+}
+
+func getHashFromData(respData string) string {
+	h := md5.New()
+	h.Write([]byte(respData))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func main() {
 	fileName := `list`
 
-	for _, line := range LinesInFile(fileName) {
-		response, err := http.Get(line)
-		if err != nil {
-			log.Fatal(err)
-		}
+	for _, site := range linesInFile(fileName) {
+		respCode, respData := getRespCodeAndSiteData(site)
+		strCode := strconv.Itoa(respCode)
+		hashedData := getHashFromData(respData)
+		outputFile := strCode + "_" + hashedData
 
-		defer response.Body.Close()
+		fmt.Printf("%d_%s_%s_%s\n", respCode, strCode, hashedData, outputFile)
 
-		httpResponseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		responseBody := string(httpResponseData)
-
-		fmt.Printf("%s", responseBody)
 	}
 }
